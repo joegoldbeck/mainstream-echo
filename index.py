28c9a582b6f7939dd52b7370cdf8b3cc68a51294
Joe Goldbeck <goldbeck.joseph@gmail.com>
2013-11-03 01:11:57 -0500

    Switch from profile API calls to artist calls

--- index.py
+++ index.py
@@ -4,8 +4,6 @@ from tornado import gen
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 from tornado.escape import json_encode, json_decode
 from urllib import urlencode
-import uuid
-import json
 
 # your api key should be stored as the first line of a file named apikey.txt
 with open('apikey.txt', 'r') as key_file:
@@ -20,78 +18,30 @@ class MainHandler(RequestHandler):
         self.write('There should be a webpage here')
         self.finish()
 
-class ArtistsHandler(RequestHandler):
+class HowMainstreamHandler(RequestHandler):
     @gen.coroutine
-    def get(self): # THIS IS TEMPORARY TO PLAY AROUND WITH
-        # create_profile_res = yield create_profile()
-        # profile_id = extract_profile_id(create_profile_res.body)
-        profile_id = 'CAYBDAK1421BD0AC59'
-        artists = ['Britney Spears', 'Radiohead', 'The Beatles']
-        add_artists_res = yield add_artists(profile_id, artists)
-        ticket = extract_ticket(add_artists_res.body)
-        print {'ticket' : ticket, 'profile_id': profile_id}
-        self.write({'ticket' : ticket, 'profile_id': profile_id})
-
-    @gen.coroutine
-    def post(self):
-        artists = json_decode(self.get_argument('artists'))
-        print artists[1] #TEST STATEMENT
-        create_profile_res = yield create_profile()
-        profile_id = extract_profile_id(create_profile_res.body)
-        add_artists_res = yield add_artists(profile_id, artists)
-        ticket = extract_ticket(add_artists_res.body)
-        self.write({'ticket' : ticket, 'profile_id': profile_id})
+    def get(self):
+        artists = ['Britney+Spears', 'Radiohead', 'The+Beatles'] # Temporarily hardcoded
+        get_profile_responses = yield [get_artist_profile(artist) for artist in artists]
+        artistProfiles = [json_decode(response.body)['response']['artist'] for
+                response in get_profile_responses]
+        hotttnesss = [profile['hotttnesss'] for profile in artistProfiles]
+        familiarity = [profile['familiarity'] for profile in artistProfiles]
+        print {'hotttnesss' : hotttnesss, 'familiarity': familiarity}
+        self.write({'hotttnesss' : hotttnesss, 'familiarity': familiarity})
 
 @gen.coroutine
-def get_artists():
+def get_artist_profile(artist):
+    req_url = ('http://developer.echonest.com/api/v4/artist/profile?api_key=' + api_key +
+            '&name=' + artist + '&bucket=familiarity&bucket=hotttnesss')
     http_client = AsyncHTTPClient()
-    response = yield http_client.fetch(
-            'http://developer.echonest.com/api/v4/artist/search?api_key=' +
-            api_key + '&sort=hotttnesss-desc&bucket=hotttnesss&results=10')
+    response = yield http_client.fetch(req_url)
     raise gen.Return(response)
-
-
-@gen.coroutine
-def create_profile():
-    req_url = 'http://developer.echonest.com/api/v4/catalog/create'
-    req_body = urlencode({'api_key': api_key, 'format': 'json',
-                            'name': uuid.uuid1().hex, 'type': 'artist'}) # random name for now
-    req = HTTPRequest(req_url, method='POST', body=req_body)
-    http_client = AsyncHTTPClient()
-    res = yield http_client.fetch(req)
-    raise gen.Return(res)
-
-
-@gen.coroutine
-def add_artists(profile_id, artists):
-    req_url = 'http://developer.echonest.com/api/v4/catalog/update'
-    data = json.dumps(artists_to_items(artists))
-    req_body = urlencode({'api_key': api_key, 'format': 'json', 'id': profile_id, 'data': data, 'data_type': 'json'})
-    print req_body
-    req = HTTPRequest(req_url, method='POST', body=req_body)
-    http_client = AsyncHTTPClient()
-    res = yield http_client.fetch(req)
-    raise gen.Return(res)
-
-
-def extract_profile_id(body):
-    parsed = json_decode(body)
-    return parsed['response']['id']
-
-def extract_ticket(body):
-    parsed = json_decode(body)
-    return parsed['response']['ticket']
-
-def artists_to_items(artists):
-    print artists
-    return [{'item': {'item_id': i, 'artist_name': artist}} for i, artist in enumerate(artists)]
 
 
 application = tornado.web.Application([
     (r'/', MainHandler), # GET webpage
-    (r'/artists', ArtistsHandler), # POST the artists list and store id & ticket in cookie
-    # (r'/status', StatusHandler), # GET the status of the profile
-    # (r'/results', ResultsHandler) # GET the results summary
+    (r'/how_mainstream', HowMainstreamHandler)
 ])
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 import unittest
 import index
-from tornado.escape import json_decode
-
-class TestExtractProfileId(unittest.TestCase):
-    def runTest(self):
-        test_body = '{"response": {"status": {"version": "4.2", "code": 0, "message": "Success"}, "type": "artist", "name": "test_artist_catalog", "id": "CATIYPU1421AC496FC"}}'
-        self.assertEqual(index.extract_profile_id(test_body), 'CATIYPU1421AC496FC')
 
 
 if __name__ == '__main__':
