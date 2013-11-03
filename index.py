80ae57422453d9c3347b4c8b7e8b09377f32d4f2
Joe Goldbeck <goldbeck.joseph@gmail.com>
2013-11-03 11:54:30 -0500

    Serve form page, usse entered values, and begin to calculate mainstreamness

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from tornado import gen
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 from tornado.escape import json_encode, json_decode
 from urllib import urlencode
+from numpy import median
 
 # your api key should be stored as the first line of a file named apikey.txt
 with open('apikey.txt', 'r') as key_file:
@@ -15,20 +16,17 @@ class MainHandler(RequestHandler):
         self._reply()
     def _reply(self):
         print self.request.method
-        self.write('There should be a webpage here')
-        self.finish()
+        self.render('index.html')
 
 class HowMainstreamHandler(RequestHandler):
     @gen.coroutine
     def get(self):
-        artists = ['Britney+Spears', 'Radiohead', 'The+Beatles'] # Temporarily hardcoded
+        artists = [artist.strip().replace(' ', '+') for artist in self.get_arguments('artist') if artist] # could also use regex re.sub(r'\s+', '+', artist.strip()) to be even safer
+        print artists
         get_profile_responses = yield [get_artist_profile(artist) for artist in artists]
         artistProfiles = [json_decode(response.body)['response']['artist'] for
                 response in get_profile_responses]
-        hotttnesss = [profile['hotttnesss'] for profile in artistProfiles]
-        familiarity = [profile['familiarity'] for profile in artistProfiles]
-        print {'hotttnesss' : hotttnesss, 'familiarity': familiarity}
-        self.write({'hotttnesss' : hotttnesss, 'familiarity': familiarity})
+        self.write((how_mainstream(artistProfiles)))
 
 @gen.coroutine
 def get_artist_profile(artist):
@@ -37,6 +35,12 @@ def get_artist_profile(artist):
     http_client = AsyncHTTPClient()
     response = yield http_client.fetch(req_url)
     raise gen.Return(response)
+
+def how_mainstream(artistProfiles):
+    hotttnessses = [profile['hotttnesss'] for profile in artistProfiles]
+    familiaritys = [profile['familiarity'] for profile in artistProfiles]
+    median_hot, median_fam = (median(hotttnessses), median(familiaritys))
+    return {'hot': median_hot, 'fam': median_fam}
 
 
 application = tornado.web.Application([
