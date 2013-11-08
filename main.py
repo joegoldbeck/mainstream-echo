3e2b8dc883635522b5e2274380ce2075fd21f181
Joe Goldbeck <goldbeck.joseph@gmail.com>
2013-11-08 11:39:12 -0500

    Slight refactor and fix caps. Begin to test format_artist_profiles

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ class HowMainstreamHandler(RequestHandler):
     def get(self):
         artists = [artist.strip().replace(' ', '+') for artist in self.get_arguments('artist') if artist] # could also use regex re.sub(r'\s+', '+', artist.strip()) to be even safer
         get_profile_responses = yield [get_artist_profile(artist) for artist in artists]
-        decodedResponses = [json_decode(response.body)['response'] for response in get_profile_responses]
-        artistProfiles = [response['artist'] for response in decodedResponses if 'artist' in response]
-        self.render('mainstream.html', snark=(how_mainstream(artistProfiles)))
+        decoded_response_bodies = [json_decode(response.body) for response in get_profile_responses]
+        artist_profiles = format_artist_profiles(decoded_response_bodies)
+        self.render('mainstream.html', snark=(how_mainstream(artist_profiles)))
 
 @gen.coroutine
 def get_artist_profile(artist):
@@ -33,11 +33,15 @@ def get_artist_profile(artist):
     response = yield http_client.fetch(req_url)
     raise gen.Return(response)
 
-def how_mainstream(artistProfiles):
-    if not artistProfiles:
+def format_artist_profiles(response_bodies):
+    response_dicts = [response_body['response'] for response_body in response_bodies]
+    return [response['artist'] for response in response_dicts if 'artist' in response]
+
+def how_mainstream(artist_profiles):
+    if not artist_profiles:
         return 'Reply hazy try again'
-    hotttnessses = [profile['hotttnesss'] for profile in artistProfiles]
-    familiaritys = [profile['familiarity'] for profile in artistProfiles]
+    hotttnessses = [profile['hotttnesss'] for profile in artist_profiles]
+    familiaritys = [profile['familiarity'] for profile in artist_profiles]
     median_hot, median_fam = (median(hotttnessses), median(familiaritys))
     if median_hot > 0.75 and median_fam > 0.75:
         return 'You may rely on it'
--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import unittest
 import main
 
+class TestFormatArtistProfiles(unittest.TestCase):
+    def test_no_artists(self):
+        self.assertEqual(main.format_artist_profiles([]), [])
+    def test_valid_artist(self):
+        valid_artist_response = [{'response': {'status': {'code': 0, 'message': 'Success', 'version': '4.2'}, 'artist': {'familiarity': 0.908821, 'hotttnesss': 0.804396, 'name': 'The Beatles', 'id': 'AR6XZ861187FB4CECD'}}}]
+        self.assertEqual(main.format_artist_profiles(valid_artist_response), [{'familiarity': 0.908821, 'hotttnesss': 0.804396, 'name': 'The Beatles', 'id': 'AR6XZ861187FB4CECD'}])
+
 class TestHowMainstream(unittest.TestCase):
-    def test_emptyList(self):
+    def test_empty_list(self):
         self.assertIsInstance(main.how_mainstream([]), str)
-    def test_validArtist(self):
+    def test_valid_artist(self):
         singleProfile = [{'familiarity': 0.908821, 'hotttnesss': 0.804396, 'name': 'The Beatles', 'id': 'AR6XZ861187FB4CECD'}]
         self.assertIsInstance(main.how_mainstream(singleProfile), str)
-    def test_validArtists(self):
+    def test_valid_artists(self):
         multipleProfiles = [{'familiarity': 0.908821, 'hotttnesss': 0.804396, 'name': 'The Beatles', 'id': 'AR6XZ861187FB4CECD'}, {'familiarity': 0.858889, 'hotttnesss': 0.797314, 'name': 'Pink Floyd', 'id': 'ARD4C1I1187FB4B0C3'}]
         self.assertIsInstance(main.how_mainstream(multipleProfiles), str)
 
